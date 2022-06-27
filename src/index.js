cc33a944ed9ba83df3264934263e92a9fffa9b26
seeoya <seeoya@naver.com>
2022-06-27 14:11:08 +0900

    초기세팅

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
 
 import "./index.css";
 
+import store from "./store/store";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        {/* <Provider store={store}> */}
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <App />
+        </BrowserRouter>
+        {/* </Provider> */}
     </React.StrictMode>
 );
 
---
+++ src/routes/Main.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+function Main() {
+    return (
+        <div id="main">
+            <div>메인 배너랑</div>
+            <div>인기템들이라거나</div>
+            <div>리뷰들이랑</div>
+            <div>서브배너들이랑</div>
+
+            <a href="">a태그</a>
+            <button>버튼</button>
+        </div>
+    );
+}
+
+export default Main;
